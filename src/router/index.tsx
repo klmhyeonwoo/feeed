72cd6030a4981bb3944ebd70b2f9b05d12d33152
klmhyeonwoo <gentlemonster77@likelion.org>
2024-06-08 22:33:00 +0900

    feat: #1 피드 서비스 구현 + Vercel 세팅

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,22 +1,29 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import DefaultLayout from "@/layout/DefaultLayout.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Error from "@/app/error";
 import Main from "@/app/main";
+import DefaultLayout from "@/layout/DefaultLayout.tsx";
 
 const routerChildren = [
-    {
-        path: '/',
-        element: <Main/>
-    }
-]
+  {
+    path: "/",
+    element: <Main />,
+  },
+];
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <DefaultLayout/>,
-        children: routerChildren
-    }
-])
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    errorElement: <Error />,
+    children: routerChildren,
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
 
 export const Router = () => {
-    return <RouterProvider router={router}/>
-}
+  return <RouterProvider router={router} />;
+};
