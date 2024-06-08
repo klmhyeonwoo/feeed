7669ae3b225c4a8724d34812f7da143e570f4d7f
klmhyeonwoo <gentlemonster77@likelion.org>
2024-06-08 23:38:03 +0900

    feat: #1 피드 서비스 구현 + GA

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+import dotenv from "dotenv";
 import { VitePluginRadar } from "vite-plugin-radar";
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
