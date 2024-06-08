3c63e632ec0289b812d3c8f93254ab09f628751d
klmhyeonwoo <gentlemonster77@likelion.org>
2024-06-08 23:30:16 +0900

    feat: #1 피드 서비스 구현 + GA

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react({jsxImportSource: '@emotion/react'})],
+  plugins: [
+    svgr(),
+    react({ jsxImportSource: "@emotion/react" }),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: process.env.VITE_GOOGLE_ANALYTICS,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
